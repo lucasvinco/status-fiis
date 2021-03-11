5e285339372a3795d735cc0547a1384d8013fb96
Lucas Vinco <lucasvinco@gmail.com>
2021-03-11 11:48:35 -0300

    added the possibility to define the door through the environment variable

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const cheerio = require('cheerio');
 const express = require('express');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
